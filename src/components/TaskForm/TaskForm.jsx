33b13d7ecabdab71286e9476e7a2261fcac2729e
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-10-25 00:03:52 +0300

    module6-lesson1

---
+++ src/components/TaskForm/TaskForm.jsx
@@ -0,0 +1,33 @@
+import { useDispatch } from 'react-redux';
+import { Button } from '../Button/Button';
+import css from './TaskForm.module.css';
+import { addTask } from '../../redux/actions';
+
+export const TaskForm = () => {
+  const dipatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    dipatch(
+      addTask({
+        id: crypto.randomUUID(),
+        completed: false,
+        text: form.elements.text.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input
+        className={css.field}
+        type="text"
+        name="text"
+        placeholder="Enter task text..."
+      />
+      <Button type="submit">Add task</Button>
+    </form>
+  );
+};
